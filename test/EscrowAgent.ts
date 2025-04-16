737c4939ee96e252d1443c38a872bf09a9d508d5
Alex <adellantado@gmail.com>
2025-04-16 12:45:32 +0300

    dispute tests

--- test/EscrowAgent.ts
+++ test/EscrowAgent.ts
@@ -216,4 +216,193 @@ import {
         });
     });
 
+    describe("Dispute agreement", () => {
+
+        async function disputedAgreementFixture() {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId } = await loadFixture(activeAgreementFixture);
+            const defaultDeadline = Number(await escrow.DEFAULT_DEADLINE_DATE());
+            await time.increase(defaultDeadline);
+            await escrow.connect(depositor).raiseDispute(agreementId);
+            return { escrow, owner, depositor, beneficiary, someone, agreementId };
+        }
+      
+        async function agreeOnArbitratorFixture() {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId } = await loadFixture(disputedAgreementFixture);
+            const feePercentage = 0.1 * 1000000;
+            const arbitrator = (await hre.ethers.getSigners())[4];
+            await escrow.connect(depositor).registerArbitrator(agreementId, arbitrator, feePercentage);
+            await escrow.connect(beneficiary).registerArbitrator(agreementId, arbitrator, feePercentage);
+            return { escrow, owner, depositor, beneficiary, someone, agreementId, arbitrator, feePercentage };
+        }
+
+        it("Depositor should dispute agreement after the deadline", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId} = await loadFixture(activeAgreementFixture);
+            const defaultDeadline = Number(await escrow.DEFAULT_DEADLINE_DATE());
+            await time.increase(defaultDeadline);
+            // check dispute raised event
+            await expect(escrow.connect(depositor).raiseDispute(agreementId)).to.emit(escrow, "DisputeRaised")
+              .withArgs(agreementId);
+            // status check
+            expect(await escrow.getAgreementStatus(agreementId)).to.be.equal(6);
+        });
+
+        it("Depositor should NOT dispute agreement before the deadline", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId} = await loadFixture(activeAgreementFixture);
+            // check revert before the deadline
+            await expect(escrow.connect(depositor).raiseDispute(agreementId)).to.revertedWith(
+              "You cannot raise dispute before the deadline"
+            );
+        });
+
+        it("Depositor and beneficiary should agree on arbitrator", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId} = await loadFixture(disputedAgreementFixture);
+            const feePercentage = 0.1 * 1000000;
+            const arbitrator = (await hre.ethers.getSigners())[4];
+            // check agreed event 
+            await expect(await escrow.connect(depositor).registerArbitrator(agreementId, arbitrator, feePercentage)).to.emit(escrow, "ArbitratorAgreed")
+              .withArgs(agreementId, arbitrator, false);
+            // check agreed event
+            await expect(await escrow.connect(beneficiary).registerArbitrator(agreementId, arbitrator, feePercentage)).to.emit(escrow, "ArbitratorAgreed")
+              .withArgs(agreementId, arbitrator, true);
+        });
+
+        it("Depositor and beneficiary should NOT agree on arbitrator's address", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId} = await loadFixture(disputedAgreementFixture);
+            const feePercentage = 0.1 * 1000000;
+            const arbitrator1 = (await hre.ethers.getSigners())[4];
+            const arbitrator2 = (await hre.ethers.getSigners())[5];
+            // check agreed event
+            await expect(await escrow.connect(depositor).registerArbitrator(agreementId, arbitrator1, feePercentage)).to.emit(escrow, "ArbitratorAgreed")
+              .withArgs(agreementId, arbitrator1, false);
+            // check revert on wrong address
+            await expect(escrow.connect(beneficiary).registerArbitrator(agreementId, arbitrator2, feePercentage)).to.revertedWith(
+              "The arbitrator address and fees should be the same"
+            );
+        });
+
+        it("Depositor and beneficiary should NOT agree on arbitrator's fees", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId} = await loadFixture(disputedAgreementFixture);
+            const feePercentage1 = 0.1 * 1000000;
+            const feePercentage2 = 0.12 * 1000000;
+            const arbitrator = (await hre.ethers.getSigners())[4];
+            // regiser an arbitrator
+            await expect(await escrow.connect(depositor).registerArbitrator(agreementId, arbitrator, feePercentage1)).to.emit(escrow, "ArbitratorAgreed")
+              .withArgs(agreementId, arbitrator, false);
+            // check revert on wrong fees
+            await expect(escrow.connect(beneficiary).registerArbitrator(agreementId, arbitrator, feePercentage2)).to.revertedWith(
+              "The arbitrator address and fees should be the same"
+            );
+        });
+
+        it("Should assign arbitrator from the pool after 'agree on arbitrator' period", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId} = await loadFixture(disputedAgreementFixture);
+            const feePercentage = 0.1 * 1000000;
+            const arbitrator = (await hre.ethers.getSigners())[4];
+            // depositor vote on arbitrator
+            await expect(await escrow.connect(depositor).registerArbitrator(agreementId, arbitrator, feePercentage)).to.emit(escrow, "ArbitratorAgreed")
+              .withArgs(agreementId, arbitrator, false);
+            const agreeOnArbitratorPeriod = Number(await escrow.AGREE_ON_ARBITRATOR_MAX_PERIOD());
+            await time.increase(agreeOnArbitratorPeriod);
+            // beneficiary vote on arbitrator after 'agree on arbitrator' period
+            const res = await escrow.connect(beneficiary).registerArbitrator(agreementId, arbitrator, feePercentage);
+            // check assigned event
+            await expect(res).to.emit(escrow, "PoolArbitratorAssigned")
+              .withArgs(agreementId, anyValue);
+            // should nor raise the event
+            await expect(res).to.not.emit(escrow, "ArbitratorAgreed");
+        });
+
+        it("Should assign arbitrator from the pool if the dispute wasn't resolved in 2 days by an agreed arbitrator", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId, arbitrator, feePercentage } = await loadFixture(agreeOnArbitratorFixture);
+            const agreeOnArbitratorPeriod = Number(await escrow.AGREE_ON_ARBITRATOR_MAX_PERIOD());
+            const resolveDisputePeriod = Number(await escrow.RESOLVE_DISPUTE_MAX_PERIOD());
+            await time.increase(agreeOnArbitratorPeriod + resolveDisputePeriod);
+            // check assigned event
+            await expect(await escrow.connect(depositor).assignArbitrator(agreementId)).to.emit(escrow, "PoolArbitratorAssigned")
+              .withArgs(agreementId, owner);
+        });
+
+        it("Should NOT assign arbitrator from the pool before 2 days after an arbitrator registered", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId, arbitrator, feePercentage } = await loadFixture(agreeOnArbitratorFixture);
+            const agreeOnArbitratorPeriod = Number(await escrow.AGREE_ON_ARBITRATOR_MAX_PERIOD());
+            await time.increase(agreeOnArbitratorPeriod);
+            // check revert prohibiting assigning arbitrator from the pool
+            await expect(escrow.connect(depositor).assignArbitrator(agreementId)).to.revertedWith(
+              "Too early to assign artibrator from the pool"
+            );
+        });
+      
+        it("Arbitrator should resolve dispute", async () => {
+          const { escrow, owner, depositor, beneficiary, someone, agreementId, arbitrator, feePercentage } = await loadFixture(agreeOnArbitratorFixture);
+            const refundPercentage = 0.1 * 1000000;
+            const value = hre.ethers.parseEther("0.1");
+            const fee = hre.ethers.parseEther("0.01");
+            const refund = hre.ethers.parseEther("0.009");
+            const released = hre.ethers.parseEther("0.081");
+            // check funds get distributed fully
+            expect(fee + refund + released).to.be.equal(value);
+            // check resolved event
+            await expect(await escrow.connect(arbitrator)["resolveDispute(uint256,uint256)"](agreementId, refundPercentage)).to.emit(escrow, "DisputeResolved")
+              .withArgs(agreementId, refundPercentage, fee, refund, released);
+            // check status 
+            expect(await escrow.getAgreementStatus(agreementId)).to.be.equal(7);
+        });
+
+        it("Depositor/beneficiary should NOT resolve dispute", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId, arbitrator, feePercentage } = await loadFixture(agreeOnArbitratorFixture);
+            const refundPercentage = 0.1 * 1000000;
+            // check revert prohibiting resolving dispute for depositor with refund percentage
+            await expect(escrow.connect(depositor)["resolveDispute(uint256,uint256)"](agreementId, refundPercentage)).to.revertedWith(
+              "You are not the arbitrator."
+            );
+            // check revert prohibiting resolving dispute for beneficiary with refund percentage
+            await expect(escrow.connect(beneficiary)["resolveDispute(uint256,uint256)"](agreementId, refundPercentage)).to.revertedWith(
+              "You are not the arbitrator."
+            );
+            // check revert prohibiting resolving dispute for depositor by splitting funds
+            await expect(escrow.connect(depositor)["resolveDispute(uint256)"](agreementId)).to.revertedWith(
+              "You can resolve dispute yourself in 2 days after the pool arbitrator assignment date"
+            );
+            // check revert prohibiting resolving dispute for beneficiary by splitting funds
+            await expect(escrow.connect(beneficiary)["resolveDispute(uint256)"](agreementId)).to.revertedWith(
+              "You can resolve dispute yourself in 2 days after the pool arbitrator assignment date"
+            );
+        });
+
+        it("Depositor/beneficiary should split escrow after assigned arbitrator isn't active for 2 days", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId, arbitrator, feePercentage } = await loadFixture(agreeOnArbitratorFixture);
+            const value = hre.ethers.parseEther("0.1");
+            const refund = value * await escrow.UNRESOLVED_DISPUTE_REFUND_PERCENTAGE() / 1_000_000n;
+            const agreeOnArbitratorPeriod = Number(await escrow.AGREE_ON_ARBITRATOR_MAX_PERIOD());
+            const resolveDisputePeriod = Number(await escrow.RESOLVE_DISPUTE_MAX_PERIOD());
+            // wait before assigning pool arbitrator
+            await time.increase(agreeOnArbitratorPeriod+resolveDisputePeriod);
+            // check assigned event
+            await expect(await escrow.connect(depositor).assignArbitrator(agreementId)).to.emit(escrow, "PoolArbitratorAssigned");
+            // wait for pool arbitrator
+            await time.increase(resolveDisputePeriod);
+            // check unresolved event
+            await expect(await escrow.connect(depositor)["resolveDispute(uint256)"](agreementId)).to.emit(escrow, "DisputeUnresolved")
+              .withArgs(agreementId, Number(await escrow.UNRESOLVED_DISPUTE_REFUND_PERCENTAGE()), refund);
+            // check status
+            expect(await escrow.getAgreementStatus(agreementId)).to.be.equal(8);
+        });
+
+        it("Depositor/beneficiary should NOT split escrow before the 2 days resolvance deadline", async () => {
+            const { escrow, owner, depositor, beneficiary, someone, agreementId, arbitrator, feePercentage } = await loadFixture(agreeOnArbitratorFixture);
+            const value = hre.ethers.parseEther("0.1");
+            const refund = value * await escrow.UNRESOLVED_DISPUTE_REFUND_PERCENTAGE() / 1_000_000n;
+            const agreeOnArbitratorPeriod = Number(await escrow.AGREE_ON_ARBITRATOR_MAX_PERIOD());
+            const resolveDisputePeriod = Number(await escrow.RESOLVE_DISPUTE_MAX_PERIOD());
+            // wait before assigning pool arbitrator
+            await time.increase(agreeOnArbitratorPeriod+resolveDisputePeriod);
+            // check assigned event
+            await expect(await escrow.connect(depositor).assignArbitrator(agreementId)).to.emit(escrow, "PoolArbitratorAssigned");
+            // check revert
+            await expect(escrow.connect(depositor)["resolveDispute(uint256)"](agreementId)).to.revertedWith(
+              "You can resolve dispute yourself in 2 days after the pool arbitrator assignment date"
+            );
+        });
+    });
+
   });
